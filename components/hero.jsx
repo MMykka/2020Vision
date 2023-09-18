42e5c608795dc4909e77ef871d33056f56134d03
Mykkaa <michaelaboukhalil@gmail.com>
2023-09-18 20:03:39 +0300

    8

--- components/hero.jsx
+++ components/hero.jsx
@@ -11,11 +11,11 @@ const Hero = () => {
         <div className=' flex flex-col items-center justify-center'>
           <p className='font-semibold text-[14px] sm:text-[15px] text-[#b6a381]'>POLARIZED SUNGLASSES</p>
           <h1 className='font-normal text-[35px] sm:text-[40px] lg:text-[50px] xl:text-[60px]'>Sandstorm Sunglasses</h1>
-         <div className='w-full h-[200px] lg:h-[230px] mt-2 object-center'>
+         <div className='w-full h-[200px] lg:h-[250px] mt-2 object-center'>
          <img src="glasses4.png" alt="" className='w-full h-full object-cover opacity-80' />
          </div>
         </div>
-        <div className='flex  gap-5 justify-center mt-20  '>
+        <div className='flex  gap-5 justify-center mt-20 lg:mt-10 '>
             <button className='px-5 py-2 text-white font-bold bg-[#ccba99] text-[13px]'>OPEN SHOP</button>
             <button className='border-solid border-b-2 font-medium text-[#555353] border-[#ccba99]'>View more</button>
           </div>
