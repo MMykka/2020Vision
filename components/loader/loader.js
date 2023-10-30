55870ea432b68ecc77df24529a95274861e034e3
Mykkaa <michaelaboukhalil@gmail.com>
2023-10-30 23:19:37 +0200

    12

--- components/loader/loader.js
+++ components/loader/loader.js
@@ -3,9 +3,9 @@ import { motion } from 'framer-motion'
 
 const Loader = () => {
   return (
-    <div className='w-full h-screen fixed bg-black z-50 text-white flex justify-center items-center'>
+    <div className='w-full h-screen mx-auto fixed bg-black z-50 text-white flex justify-center items-center'>
      <motion.div
-        className=" w-full sm:w-[200px]">
+        className=" w-[100px] sm:w-[200px]">
           <img src="gif.gif" className="w-full h-full" />
         </motion.div>
     </div>
